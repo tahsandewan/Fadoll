4aaa26561f56c02f5093a90ac9c18b45a4361307
tahsandewan <tahsandewan@gmail.com>
2020-03-28 20:22:20 +0600

    details

--- src/home/Details.js
+++ src/home/Details.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+
 class Details extends Component {
   state = {
     loading: true,
@@ -31,7 +34,32 @@ class Details extends Component {
   render() {
     return (
       <div>
-        <h1>image:{this.state.products.ProductPrice}</h1>
+        {/* <h1>image:{this.state.products.ProductPrice}</h1> */}
+        <div className="left">
+          <p>{this.state.products.ProductImage}</p>
+        </div>
+        <div className="right">
+          <h1>{this.state.products.ProductName}</h1>
+          <div className="tabname">
+            <Tabs>
+              <TabList>
+                <Tab>ITEM DESCRIPTION</Tab>
+                <Tab>PRODUCT INFORMATION</Tab>
+                <Tab> RETURN & EXCHANGE</Tab>
+              </TabList>
+
+              <TabPanel>
+                <p>{this.state.products.description}</p>
+              </TabPanel>
+              <TabPanel>
+                <p>{this.state.products.caption}</p>
+              </TabPanel>
+              <TabPanel>
+                <p>{this.state.products.ModifiedBy}</p>
+              </TabPanel>
+            </Tabs>
+          </div>
+        </div>
       </div>
     );
   }
